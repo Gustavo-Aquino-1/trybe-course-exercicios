af1347f97e8dd33be29c45b3fcab8c22a62b6ac0
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-06-21 10:28:29 -0300

    4 primeiros ex feitos

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,33 @@
+//1
+const a = 10;
+const b = 4;
+
+let soma = a + b;
+let sub = a - b;
+let multi = a * b;
+let div = a / b;
+let mod = a % b;
+
+//2
+const n1 = 52
+const n2 = 50;
+if(n1 > n2){
+    console.log(n1)
+}else{
+    console.log(n2)
+}
+
+//3
+const nu1 = 13;
+const nu2 = 87;
+const nu3 = 54;
+
+if(nu1 > nu2 && nu1 > nu3){
+    console.log(nu1)
+}else if(nu2 > nu1 && nu2 > nu3){
+    console.log(nu2)
+}else{
+    console.log(nu3)
+}
+
+//4
